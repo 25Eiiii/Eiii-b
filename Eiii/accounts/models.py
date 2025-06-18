f5dcf8559fc717a760e4bb8645f1e0f0c6b69ddb
Wish <asgvcx@naver.com>
2025-06-19 00:44:31 +0900

    feat: 프로필 입력API 구현
    related to #1

--- Eiii/accounts/models.py
+++ Eiii/accounts/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+from django.conf import settings #프로파일 모델용
 # Create your models here.
 
 class CustomUserManager(BaseUserManager):
@@ -35,3 +36,19 @@ class CustomUser(AbstractBaseUser):
     @property
     def is_staff(self):
         return self.is_admin
+
+class Profile(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+    major = models.CharField(max_length=100) #학과
+    year = models.CharField(max_length=10) #학년
+    preferred_gender = models.CharField(max_length=20) # 성별선호   
+    dining_style = models.CharField(max_length=20) #식사 스타일
+    eating_speed = models.CharField(max_length=20) #식사 속도
+    meal_purpose = models.CharField(max_length=20) #밥약 목적
+    dessert = models.CharField(max_length=20) # 디저트 여부
+    preferred_menu = models.JSONField()  #선호메뉴(다중 선택으로)
+    dietary_restrictions = models.JSONField()  #특이사항(다중 선택으로)
+
+    def __str__(self):
+        return f"{self.user.username}'s profile"
