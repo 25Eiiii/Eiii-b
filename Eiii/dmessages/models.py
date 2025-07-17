17d7d753b3793a5796c392c0f6fcc27c52414e92
Wish <asgvcx@naver.com>
2025-07-17 16:37:53 +0900

    feat: 쪽지 기능 완성
    related to #11

--- Eiii/dmessages/models.py
+++ Eiii/dmessages/models.py
@@ -2,6 +2,10 @@ from django.db import models
 from django.conf import settings
 
 # Create your models here.
+class ChatRoom(models.Model):
+    participants = models.ManyToManyField(settings.AUTH_USER_MODEL)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
 class Message(models.Model):
     sender = models.ForeignKey(
         settings.AUTH_USER_MODEL, related_name='sent_messages',
@@ -17,5 +21,9 @@ class Message(models.Model):
     is_request = models.BooleanField(default=False)  # 대화 신청용 메시지 여부
     is_accepted = models.BooleanField(null=True, blank=True)  # 수락/거절 여부 저장
 
+    chatroom = models.ForeignKey(ChatRoom, null=True, blank=True, on_delete=models.CASCADE)
+
     def __str__(self):
         return f"{self.sender} ➝ {self.receiver}: {self.content[:20]}"
+    
+
